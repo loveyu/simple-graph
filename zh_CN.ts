25728e940da6d6e3c730511cf7b1c5310b752c6d
loveyu <admin@loveyu.info>
2014-12-26 12:26:05 +0800

    add translate file

---
+++ zh_CN.ts
@@ -0,0 +1,337 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>GraphTool</name>
+    <message>
+        <location filename="graphtool.cpp" line="30"/>
+        <source>Radius:</source>
+        <translation>边框：</translation>
+    </message>
+    <message>
+        <location filename="graphtool.cpp" line="31"/>
+        <source>Radius</source>
+        <translation>边框</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Graph Edit</source>
+        <translation>图像编辑</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <source>Draw</source>
+        <translation>绘图</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="71"/>
+        <source>GraphEdit</source>
+        <translation>图像编辑</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>VectorEdit</source>
+        <translation>向量编辑</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <source>3D</source>
+        <translation>3D</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <source>Transform</source>
+        <translation>变换</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="109"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="140"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="145"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="150"/>
+        <source>Line</source>
+        <translation>线</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="155"/>
+        <source>Circle</source>
+        <translation>圆</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="160"/>
+        <source>Oval</source>
+        <translation>椭圆</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="165"/>
+        <location filename="mainwindow.ui" line="168"/>
+        <location filename="mainwindow.cpp" line="57"/>
+        <source>Line Cut</source>
+        <translation>线段裁剪</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="173"/>
+        <location filename="mainwindow.cpp" line="90"/>
+        <source>Fill</source>
+        <translation>填充</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="178"/>
+        <location filename="mainwindow.cpp" line="63"/>
+        <source>Revolve</source>
+        <translation>旋转</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="183"/>
+        <source>Polygon</source>
+        <translation>多边形</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="188"/>
+        <location filename="mainwindow.ui" line="233"/>
+        <location filename="mainwindow.cpp" line="66"/>
+        <source>Scaled</source>
+        <translation>缩放</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="193"/>
+        <source>Cube</source>
+        <translation>正方体</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="198"/>
+        <source>Triangle</source>
+        <translation>三角形</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="203"/>
+        <location filename="mainwindow.ui" line="223"/>
+        <location filename="mainwindow.cpp" line="60"/>
+        <source>Move</source>
+        <translation>移动</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="208"/>
+        <location filename="mainwindow.cpp" line="82"/>
+        <source>Erase</source>
+        <translation>擦除</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="213"/>
+        <source>Clear</source>
+        <translation>清除</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="218"/>
+        <location filename="mainwindow.ui" line="258"/>
+        <source>Rect</source>
+        <translation>矩形</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="228"/>
+        <source>Rotate</source>
+        <translation>旋转</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="238"/>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="243"/>
+        <location filename="mainwindow.cpp" line="225"/>
+        <source>ToBitmap</source>
+        <translation>转为位图</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <location filename="mainwindow.cpp" line="247"/>
+        <source>To3D</source>
+        <translation>转为3D</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="253"/>
+        <source>Vertebral</source>
+        <translation>椎体</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="263"/>
+        <source>Author</source>
+        <translation>作者</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="268"/>
+        <source>Info</source>
+        <translation>信息</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="35"/>
+        <source>Normal</source>
+        <translation>正常</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="39"/>
+        <source>Draw Line</source>
+        <translation>画线</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="43"/>
+        <source>Draw Circle</source>
+        <translation>画圆</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="47"/>
+        <source>Draw Triangle</source>
+        <translation>画三角形</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <source>Draw Polygon</source>
+        <translation>画多边形</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="54"/>
+        <source>Draw Oval</source>
+        <translation>画椭圆</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="69"/>
+        <source>Matrix Reset</source>
+        <translation>矩阵重置</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="72"/>
+        <source>Matrix Move</source>
+        <translation>矩阵移动</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="75"/>
+        <source>Matrix Rotate</source>
+        <translation>矩阵旋转</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="78"/>
+        <source>Matrix Scaled</source>
+        <translation>矩阵缩放</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>3D Mode</source>
+        <translation>3D模式</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="99"/>
+        <source>3D Cube</source>
+        <translation>3D正方体</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>!!!!warning!!!! unknown action</source>
+        <translation>！警告！未知操作</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="185"/>
+        <source>Save File</source>
+        <translation>保存文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="185"/>
+        <source>Image (*.png)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="191"/>
+        <source>Save success</source>
+        <translation>保存成功</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="193"/>
+        <source>Save error</source>
+        <translation>保存错误</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="193"/>
+        <source>Save error
+Please try again!</source>
+        <translation>保存错误
+请重试一次！</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="222"/>
+        <source>ToVector</source>
+        <translation>转为向量</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="253"/>
+        <source>To2D</source>
+        <translation>转为2D</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="273"/>
+        <source>Action error</source>
+        <translation>操作错误</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="273"/>
+        <source>Action error.
+Must swicth to 3D mode.</source>
+        <translation>操作错误。
+必须先转换为3D模式。</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="294"/>
+        <source>About the author</source>
+        <translation>关于作者</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="294"/>
+        <source>loveyu
+http://www.loveyu.org/</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="299"/>
+        <source>About This Programe</source>
+        <translation>关于程序</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="299"/>
+        <source>A simple basic image rendering tool,
+ supports several 3D presentations.</source>
+        <translation>一个简单的基本图像绘制工具，
+支持几种3D的演示。</translation>
+    </message>
+</context>
+<context>
+    <name>PainterWidget</name>
+    <message>
+        <location filename="painterwidget.cpp" line="28"/>
+        <source>Action stop</source>
+        <translation>操作停止</translation>
+    </message>
+    <message>
+        <location filename="painterwidget.cpp" line="28"/>
+        <source>You must convert to bitmap image.</source>
+        <translation>你必须转为位图再操作。</translation>
+    </message>
+</context>
+</TS>
